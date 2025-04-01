27ef37e5dadf9d2a3e05d18626fe283407607460
olaficpex <olaficpex@users.noreply.github.com>
2025-04-01 14:44:10 +0800

    init

---
+++ src/common/did.rs
@@ -0,0 +1,38 @@
+#[candid::candid_method(query)]
+#[cfg(test)]
+fn __get_candid_interface_tmp_hack() -> String {
+    todo!()
+}
+
+#[ic_cdk::query]
+#[cfg(not(test))]
+fn __get_candid_interface_tmp_hack() -> String {
+    #[allow(unused_imports)]
+    use crate::types::*;
+
+    candid::export_service!();
+    __export_service()
+}
+
+// =========== print did ===========
+
+/// `cargo test update_candid -- --nocapture`
+#[test]
+fn update_candid() {
+    #[allow(unused_imports)]
+    use crate::types::*;
+
+    candid::export_service!();
+
+    let text = __export_service();
+
+    // std::println!("{}", text);
+
+    use std::io::Write;
+    let filename = "sources/source.did";
+    let _ = std::fs::remove_file(filename);
+    std::fs::File::create(&filename)
+        .expect("create failed")
+        .write_all(text.as_bytes())
+        .expect("write candid failed");
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![doc = include_str!("../README.md")]
+// #![deny(unreachable_pub)]
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+#![warn(rustdoc::broken_intra_doc_links)]
+#![warn(clippy::future_not_send)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+
+mod types;
+
+mod common; // last
