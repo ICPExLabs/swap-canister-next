74f0725ae1b7dd886f2fd7c6943f5c8962a6bbdc
kalamansi <kalamansi@users.noreply.github.com>
2025-05-09 23:41:15 +0800

    test: swap

--- canisters/swap/tests/test-all.sh
+++ canisters/swap/tests/test-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sh tests/test-archive-token.sh update
+sh canisters/swap/tests/test-archive-token.sh update
 
-sh tests/test-archive-swap.sh update
+sh canisters/swap/tests/test-archive-swap.sh update
 
-sh tests/test-swap.sh update
+sh canisters/swap/tests/test-swap.sh update
